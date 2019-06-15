90d844e5ffb6d76359190643e8d4d9d46455a6bb
Sarthak Sharma <31410305+InvincibleSarthak@users.noreply.github.com>
2019-06-15 23:13:13 +0530

    Create Euler Totient Function.cpp

---
+++ Euler Totient Function.cpp
@@ -0,0 +1,40 @@
+/*In number theory, the totient φ of a positive integer n is defined to be the number of positive integers less than or equal to n that are coprime to n.
+
+Given an integer n (1 <= n <= 10^6). Compute the value of the totient φ.
+Input
+
+First line contains an integer T, the number of test cases. (T <= 20000)
+
+T following lines, each contains an integer n.
+Output
+
+T lines, one for the result of each test case.*/
+
+
+
+#include <iostream>
+using namespace std;
+#define ll long long
+const int N=1000002;
+ll arr[N];
+int main() {
+	for (ll i=1;i<N;i++){
+	    arr[i]=i;
+	}
+	for(ll i=2;i<N;i++){
+	    if(arr[i]==i){
+	        arr[i]=-1;
+	        for(ll j=i;j<N;j+=i){
+	            arr[j]=(arr[j]*(i-1))/i;
+	        }
+	    }
+	}
+	ll t;
+	ll n;
+	cin>>t;
+	while(t--){
+	    cin>>n;
+	    cout<<arr[n];
+	}
+	return 0;
+}
