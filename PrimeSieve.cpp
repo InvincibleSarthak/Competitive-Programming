a941ea56b296d79f076d493b8321200c21ed74a1
Sarthak Sharma <31410305+InvincibleSarthak@users.noreply.github.com>
2019-06-19 01:56:31 +0530

    Create PrimeSieve.cpp

---
+++ PrimeSieve.cpp
@@ -0,0 +1,35 @@
+//Basic program to print all the prime numbers till n.
+
+
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+const ll N=100000002;
+ll arr[N];
+void primeCheck(ll n){
+    arr[0]=arr[1]=0;
+    arr[2]=1;
+    for(ll i=3;i<=n;i+=2){
+        arr[i]=1;
+        arr[i+1]=0;
+    }
+    for(ll i=3;i<=n;i+=2){
+        if(arr[i]==1){
+            for(ll j=i*i;j<=n;j+=2*i){
+                arr[j]=0;
+            }
+        }
+    }
+    return;
+}
+int main(){
+    ll n;
+    cin>>n;
+    primeCheck(n);
+    for(ll i=2;i<=n;i++){
+        if(arr[i]==1)
+            cout<<i<<' ';
+    }
+    return 0;
+}
