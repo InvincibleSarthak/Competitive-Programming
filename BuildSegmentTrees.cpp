3f6d4f3a571b6b5a28dd81b29bb2f0ad59f0c58c
Sarthak Sharma <31410305+InvincibleSarthak@users.noreply.github.com>
2019-06-25 12:54:49 +0530

    Update BuildSegmentTrees.cpp

--- BuildSegmentTrees.cpp
+++ BuildSegmentTrees.cpp
@@ -1,8 +1,23 @@
 //To print the minimum number in the index range qs and qe using the concept of segment trees.
+//To update a number present at index i by incrementing inc in it. 
 
 
 #include<iostream>
 using namespace std;
+void updateTree(int *tree, int ss, int se, int i, int inc, int index){
+    if(ss==se){
+        tree[index]+=inc;
+        return;
+    }
+    if(i>se || i<ss){
+        return;
+    }
+    int mid=(ss+se)/2;
+    updateTree(tree,ss,mid,i,inc,2*index);
+    updateTree(tree,mid+1,se,i,inc,2*index+1);
+    tree[index]=min(tree[2*index],tree[2*index+1]);
+    return;
+}
 int query(int ss, int se, int qs, int qe, int *tree, int index){
     //No overlap
     if(ss>qe || se<qs){
@@ -40,10 +55,17 @@ int main(){
     }
     int *tree= new int [4*n+1];
     buildTree(arr,0,n-1,tree,1);
-    int qs,qe;
-    cin>>qs>>qe;
-    for(int i=1;i<(4*n+1);i++){
+    //int qs,qe;
+    //cin>>qs>>qe;
+    //for(int i=1;i<(4*n+1);i++){
     //    cout<<tree[i]<<' ';
+    //}
+    //cout<<query(0,n-1,qs,qe,tree,1)<<endl;
+    int i,inc;
+    cin>>i>>inc;
+    updateTree(tree,0,n-1,i,inc,1);
+    for(int i=1;i<(4*n+1);i++){
+        cout<<tree[i]<<' ';
     }
-    cout<<query(0,n-1,qs,qe,tree,1)<<endl;
+    
 }
