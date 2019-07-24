d9323d0311aae100eca2f058e7542dc8a2713835
Sarthak Sharma <31410305+InvincibleSarthak@users.noreply.github.com>
2019-07-24 09:27:54 +0530

    Update LazyQuery.cpp

--- Segment Trees/LazyQuery.cpp
+++ Segment Trees/LazyQuery.cpp
@@ -1,62 +1,71 @@
 //My version of lazy propagation in segment trees
 
 
+#define ll long long int
 #include<iostream>
 using namespace std;
-void updateLazy(int *lazy, int *tree, int index, int low, int high, int qlow, int qhigh,int val){
-    //No overlap
-    if(qlow>high || qhigh<low){
-        return ;
-    }
-    //complete overlap
-    if(qlow<=low && qhigh>=high){
-        if(low==high){
-            tree[index]+=val;
-            return ;
-        }
-        lazy[index]+=val;
+ll arr[100005];
+ll tree[400021];
+ll lazy[400021];
+void updateLazy(ll index, ll low, ll high, ll qlow, ll qhigh, ll val){
+    //LazyNode
+    if(lazy[index]!=0){
         tree[index]+=lazy[index];
-        lazy[2*index]+=lazy[index];
-        lazy[2*index+1]+=lazy[index];
+        //If not at leaf node, solve for children
+        if(low!=high){
+            lazy[2*index]+=lazy[index];
+            lazy[2*index+1]+=lazy[index];
+        }
         lazy[index]=0;
-        return ;
     }
-    //Partial overlap
-    int mid = (low+high)/2;
-    updateLazy(lazy,tree,2*index,low,mid,qlow,qhigh,val);
-    updateLazy(lazy,tree,2*index+1,mid+1,high,qlow,qhigh,val);
-    tree[index]=min(tree[2*index],tree[2*index+1]);
-}
-void buildTree(int *arr, int low, int high, int *tree, int index){
-    if(low==high){
-        tree[index]=arr[low];
+    //Overflow
+    if(low>high || qlow>high || qhigh<low){
         return;
     }
-    int mid =(low+high)/2;
-    buildTree(arr,low,mid,tree,2*index);
-    buildTree(arr,mid+1,high,tree,2*index+1);
+    //Complete Overlap
+    if(low>=qlow && high<=qhigh){
+        tree[index]+=val;
+        if(low!=high){
+            lazy[2*index]+=val;
+            lazy[2*index+1]+=val;
+        }
+        return;
+    }
+    //Partial Overlap
+    ll mid = (low+high)>>1;
+    updateLazy(2*index,low,mid,qlow,qhigh,val);
+    updateLazy(2*index+1,mid+1,high,qlow,qhigh,val);
     tree[index]=min(tree[2*index],tree[2*index+1]);
-    return;
+    return ;
+}
+void buildTree(ll index, ll low, ll high){
+    //Leaf Node
+    if(low==high){
+        tree[index]=arr[low];
+        return ;
+    }
+    ll mid = (low+high)>>1;
+    buildTree(2*index,low,mid);
+    buildTree(2*index+1,mid+1,high);
+    tree[index]=min(tree[2*index],tree[2*index+1]);
+    return ;
 }
 int main(){
-    int n;
+    ll n;
     cin>>n;
-    int arr[n];
-    for(int i=1;i<=n;i++){
+    for(ll i=1;i<=n;i++){
         cin>>arr[i];
     }
-    int tree[4*n+1];
-    int lazy[4*n+1];
-    for(int i=0;i<(4*n+1);i++){
-        lazy[i]=0;
-    }
-    buildTree(arr,1,n,tree,1);
-    int l,r;
-    cin>>l>>r;
-    int val;
-    cin>>val;
-    updateLazy(lazy,tree,1,1,n,l,r,val);
-    for(auto it:tree){
-        cout<<it<<" ";
+    buildTree(1,1,n);
+    ll t;
+    cin>>t;
+    while(t--){
+        ll qlow,qhigh,val;
+        cin>>qlow>>qhigh>>val;
+        updateLazy(1,1,n,qlow,qhigh,val);
+        for(ll i=1;i<2*n;i++){
+            cout<<tree[i]<<" ";
+        }
+        cout<<endl;
     }
 }
