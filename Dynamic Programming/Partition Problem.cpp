caa5eb32a198173eb484ac5338a1d592ab9ba86f
Sarthak Sharma <31410305+InvincibleSarthak@users.noreply.github.com>
2019-08-20 20:23:25 +0530

    Create Partition Problem.cpp

---
+++ Dynamic Programming/Partition Problem.cpp
@@ -0,0 +1,48 @@
+// To check whether it is possible to divide the given set into two equal subsets. Equal means their sum must be same.
+
+#include<iostream>
+using namespace std;
+bool solve(int *arr, int n){
+    int sum = 0;
+    for(int i=0;i<n;i++){
+        sum += arr[i];
+    }
+    if(sum%2 != 0){
+        return false; 
+    }
+    else{
+        bool dp[n+1][(sum/2)+1];
+        dp[0][0] = true;
+        for(int i=0;i<=(sum/2);i++){
+            dp[0][i] = false;
+        }
+        for(int i=0;i<=n;i++){
+            dp[i][0] = true;
+        }
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=(sum/2);j++){
+                dp[i][j] = dp[i-1][j];
+                if(arr[i] <= j){
+                    dp[i][j] = dp[i][j] || dp[i-1][j-arr[i]];
+                }
+            }
+        }
+        return dp[n][sum/2];
+    }
+    
+}
+int main(){
+    int n;
+    cin>>n;
+    int arr[n];
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    bool ans = solve(arr,n);
+    if(ans){
+        cout<<"Division is possible!";
+    }
+    else{
+        cout<<"Division not possible!";
+    }
+}
