4a9156c4edd4d06eacc02835f89544e5f071baa0
Sarthak Sharma <31410305+InvincibleSarthak@users.noreply.github.com>
2019-08-23 11:55:19 +0530

    Create OptimalGameStrategy.cpp

---
+++ Dynamic Programming/OptimalGameStrategy.cpp
@@ -0,0 +1,32 @@
+//Player1 and Player2 are equally smart and are allowed to pick the number either from start or from the end. Our aim is to print the
+//maximum possible win difference between them.
+
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+int arr[1000];
+int dp[1000][1000];
+int solve(int i, int j){
+    if(i>=j){
+        return 0;
+    }
+    if(dp[i][j] != -1){
+        return dp[i][j];
+    }
+    int q1 = arr[i]+min(solve(i+2,j) , solve(i+1,j-1));
+    int q2 = arr[j]+min(solve(i,j-2) , solve(i+1,j-1));
+    dp[i][j] = max(q1,q2);
+    return dp[i][j];
+}
+int main(){
+    memset(dp,-1,sizeof(dp));
+    int n;
+    int sum = 0;
+    cin>>n;
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+        sum += arr[i];
+    }
+    int ans = solve(0,n-1);
+    cout<<2*ans - sum<<" ";
+}
