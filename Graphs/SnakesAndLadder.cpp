7bb0a347901bf8be8eb5c11670c6ae5cfe3e8c1e
Sarthak Sharma <31410305+InvincibleSarthak@users.noreply.github.com>
2020-05-20 23:59:42 +0530

    Update SnakesAndLadder.cpp

--- Graphs/SnakesAndLadder.cpp
+++ Graphs/SnakesAndLadder.cpp
@@ -1,67 +1,56 @@
-//Given a 6*6 snakes and ladder game
-//It will print the minimum number of moves required to win the game and the path as well
-
 #include<iostream>
 #include<bits/stdc++.h>
 using namespace std;
 template<typename T>
 class Graph{
-    map<T,vector<T>>adjList;
+    map<int , vector<int> > adjList;
     public:
-    void addEdge(T a, T b, bool bidir = false){
+    void addEdge(T a, T b){
         adjList[a].push_back(b);
-        if(bidir) adjList[b].push_back(a);
     }
-    int bfs(T src, int dest){
-        map<T,int>dist;
-        queue<T>q;
-        map<T,T>parent;
-        for(auto it:adjList){
-            dist[it.first]=INT_MAX;
+    void bfs(T src, T dest){
+        map<int, int> visited;
+        queue<int>q;
+        for(auto it: adjList){
+            visited[it.first] = INT_MAX;
         }
+        visited[src]=0;
+        T node;
         q.push(src);
-        dist[src]=0;
         while(!q.empty()){
-            T node = q.front();
+            node = q.front();
             q.pop();
             for(auto it: adjList[node]){
-                if(dist[it]==INT_MAX){
+                if(visited[it] == INT_MAX){
                     q.push(it);
-                    dist[it]=dist[node]+1;
-                    parent[it]=node;
+                    visited[it] = visited[node]+1;
                 }
             }
         }
-        T temp = dest;
-        while(temp!=src){
-            cout<<temp<<" <- ";
-            temp=parent[temp];
-        }
-        cout<<"0"<<endl;
-        return dist[dest];
+        cout<<visited[dest]<<'\n';
     }
 };
 int main(){
     Graph<int>g;
-    int board[50]={0};
-    board[2]=13;
-    board[5]=2;
-    board[9]=18;
-    board[17]=-13;
-    board[18]=11;
-    board[20]=-14;
-    board[25]=10;
-    board[32]=-2;
-    board[34]=-22;
-    for(int a=0;a<=36;a++){
-        for(int b=1;b<=6;b++){
-            g.addEdge(a,a+b+board[a+b]);
+    int board[37]={0};
+    board[2] = 13;
+    board[5] = 2;
+    board[9] = 18;
+    board[18] = 11;
+    board[25] = 10;
+    board[17] = -13;
+    board[20] = -14;
+    board[24] = -8;
+    board[32] = -2;
+    board[34] = -22;
+    for(int i=0;i<37;i++){
+        for(int dice=1;dice<=6;dice++){
+            int j = i+dice;
+            j += board[j];
+            if(j<=36){
+                g.addEdge(i,j);
+            }
         }
     }
-    cout<<g.bfs(0,36);
+    g.bfs(0,36); 
 }
-
-/*
-36 <- 30 <- 29 <- 15 <- 0
-4
-*/
