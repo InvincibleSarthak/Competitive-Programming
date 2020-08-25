91d80594ce42dba5ebcc406da9e091b4545c0e63
Sarthak Sharma <31410305+InvincibleSarthak@users.noreply.github.com>
2020-08-25 12:16:33 +0530

    Update LongestCommonSubsequence.cpp

--- Dynamic Programming/LongestCommonSubsequence.cpp
+++ Dynamic Programming/LongestCommonSubsequence.cpp
@@ -1,30 +1,46 @@
-//To find the number of letters present in the longest common sub sequence of the two strings given by the user
+//Given: Two strings 
+//To print the longest common subsequence
 
 #include<iostream>
+#include<bits/stdc++.h>
 using namespace std;
-int lis(int arr[], int n){
-    int dp[n];
-    for(int i=0;i<n;i++){
-        dp[i]=1;
+int main(){
+    string s1,s2;
+    cin>>s1>>s2;
+    int m = s1.size();
+    int n = s2.size();
+    int dp[m+1][n+1];
+    for(int i=0;i<=m;i++){
+        dp[i][0] = 0;
     }
-    int best = -1;
-    for(int i=1;i<n;i++){
-        for(int j=0;j<i;j++){
-            if(arr[j]<=arr[i]){
-                int count = 1+dp[j];
-                dp[i]=max(dp[i],count);
+    for(int j=0;j<=n;j++){
+        dp[0][j] = 0;
+    }
+    for(int i=1;i<=m;i++){
+        for(int j=1;j<=n;j++){
+            if(s1[i-1] == s2[j-1]){
+                dp[i][j] = dp[i-1][j-1] + 1;
+            }
+            else{
+                dp[i][j] = max(dp[i-1][j] , dp[i][j-1]);
             }
         }
-        best = max(best,dp[i]);
     }
-    return best;
-}
-int main(){
-    int n;
-    cin>>n;
-    int arr[n];
-    for(int i=0;i<n;i++){
-        cin>>arr[i];
+    int a=m,b=n;
+    string ans;
+    while(a>0 || b>0){
+        if(s1[a-1] == s2[b-1]){
+            ans += s1[a-1];
+            a--;
+            b--;
+        }
+        else if(dp[a-1][b] > dp[a][b-1]){
+            a--;
+        }
+        else{
+            b--;
+        }
     }
-    cout<<lis(arr,n);
+    reverse(ans.begin(),ans.end());
+    cout<<ans;
 }
