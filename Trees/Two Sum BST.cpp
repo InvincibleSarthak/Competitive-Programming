b5eae314941c91fb039cc970e794c470fc3f2c2c
Sarthak Sharma <31410305+InvincibleSarthak@users.noreply.github.com>
2020-10-10 19:21:12 +0530

    iterative and recursive

--- Trees/Two Sum BST.cpp
+++ Trees/Two Sum BST.cpp
@@ -1,3 +1,5 @@
+//Approach 1 : In-order Traversal 
+// time: O(n) space: O(n)
 class Solution {
     int sum;
     bool ans = false;
@@ -20,3 +22,56 @@ public:
         return ans;
     }
 };
+
+
+//Approach 2: Iterative
+//time: O(n) space: O(h)
+int Solution::t2Sum(TreeNode* head, int k) {
+    TreeNode* start = head, *end = head, *temp = head;
+    stack<TreeNode*>sleft,sright;
+    while(temp){
+        sleft.push(temp);
+        temp = temp->left;
+    }
+    start = sleft.top();
+    temp = head;
+    while(temp){
+        sright.push(temp);
+        temp = temp->right;
+    }
+    end = sright.top();
+    
+    int sum;
+    while(start != end){
+        sum = start->val + end->val;
+        if(sum == k){
+            return 1;
+        }
+        else if(sum < k){
+            temp = sleft.top();
+            sleft.pop();
+            if(temp->right){
+                temp = temp->right;
+                while(temp){
+                    sleft.push(temp);
+                    temp = temp->left;
+                }
+            }
+            start = sleft.top();
+        }
+        else{
+            temp = sright.top();
+            sright.pop();
+            if(temp->left){
+                temp = temp->left;
+                while(temp){
+                    sright.push(temp);
+                    temp = temp->right;
+                }
+            }
+            end = sright.top();
+        }
+        
+    }
+    return 0;
+}
