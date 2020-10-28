d0e9ee5350fdc09a4a12e105d3b0bec9e9e803ce
Sarthak Sharma <31410305+InvincibleSarthak@users.noreply.github.com>
2020-10-28 20:14:16 +0530

    Two pointer approach

---
+++ Arrays/Three way partitioning.cpp
@@ -0,0 +1,22 @@
+vector<int> threeWayPartition(vector<int> arr, int a, int b)
+{
+    int n = arr.size();
+    int start = 0, end = n-1;
+    
+    for(int i=0;i<=end;){
+        if(arr[i] < a){
+            swap(arr[i],arr[start]);
+            i++,start++;
+        }
+        else if(arr[i] > b){
+            swap(arr[i],arr[end]);
+            end--;
+        }
+        else{
+            i++;
+        }
+    }
+    
+    return arr;
+    
+}
