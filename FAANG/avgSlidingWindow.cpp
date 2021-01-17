847e9a97fbd5914dc261fa322a78b60bb601a9e9
Sarthak Sharma <31410305+InvincibleSarthak@users.noreply.github.com>
2021-01-17 21:31:59 +0530

    using Queue

--- FAANG/avgSlidingWindow.cpp
+++ FAANG/avgSlidingWindow.cpp
@@ -1,3 +1,26 @@
+/*
+You have to given a data stream terminated by -1 and the size of sliding window. For each variation in sliding window you need to tell the average of data in current sliding 
+window. Print 4 digits after the decimal point.
+
+Input Format
+First line contains an integer denoting the size of sliding window.
+Second line has a data stream terminated by -1.
+
+Constraints
+None
+
+Output Format
+Print average of each sliding window.
+
+Sample Input
+5
+51 62 24 51 79 33 72 78 84 42 -1
+Sample Output
+51.0000 56.5000 45.6667 47.0000 53.4000 49.8000 51.8000 62.6000 69.2000 61.8000 
+*/
+
+
+
 #include <iostream>
 #include <bits/stdc++.h>
 using namespace std;
